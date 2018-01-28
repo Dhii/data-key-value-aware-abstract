f8cf41401f8741f7345758f969b277402e73b12b
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-28 22:53:01 +0100

    Tests brought up to standard

--- test/functional/KeyAwareTraitTest.php
+++ test/functional/KeyAwareTraitTest.php
@@ -2,47 +2,162 @@
 
 namespace Dhii\Data\FuncTest;
 
-use Dhii\Data\KeyAwareTrait;
+
+use Dhii\Data\KeyAwareTrait as TestSubject;
+
 use Xpmock\TestCase;
+use Exception as RootException;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use PHPUnit_Framework_MockObject_MockBuilder as MockBuilder;
 
 /**
- * Tests {@see Dhii\Data\KeyAwareTrait}.
+ * Tests {@see TestSubject}.
  *
  * @since [*next-version*]
  */
 class KeyAwareTraitTest extends TestCase
 {
     /**
-     * The name of the test subject.
+     * The class name of the test subject.
      *
      * @since [*next-version*]
      */
-    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Data\\KeyAwareTrait';
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Data\KeyAwareTrait';
 
     /**
      * Creates a new instance of the test subject.
      *
      * @since [*next-version*]
      *
-     * @return KeyAwareTrait
+     * @param array $methods The methods to mock.
+     *
+     * @return MockObject The new instance.
      */
-    public function createInstance()
+    public function createInstance($methods = [])
     {
-        return $this->getMockForTrait(static::TEST_SUBJECT_CLASSNAME);
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+            '__',
+        ]);
+
+        $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+            ->setMethods($methods)
+            ->getMockForTrait();
+
+        $mock->method('__')
+                ->will($this->returnArgument(0));
+
+        return $mock;
     }
 
     /**
-     * Tests the key getter and setter methods.
+     * Merges the values of two arrays.
+     *
+     * The resulting product will be a numeric array where the values of both inputs are present, without duplicates.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $destination The base array.
+     * @param array $source      The array with more keys.
+     *
+     * @return array The array which contains unique values
+     */
+    public function mergeValues($destination, $source)
+    {
+        return array_keys(array_merge(array_flip($destination), array_flip($source)));
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * This is particularly useful for cases where the mock is based on an
+     * internal class, such as in the case with exceptions. Helps to avoid
+     * writing hard-coded stubs.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $className      Name of the class for the mock to extend.
+     * @param string $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return MockBuilder The builder for a mock of an object that extends and implements
+     *                     the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf('abstract class %1$s extends %2$s implements %3$s {}', [
+            $paddingClassName,
+            $className,
+            implode(', ', $interfaceNames),
+        ]);
+        eval($definition);
+
+        return $this->getMockForAbstractClass($paddingClassName);
+    }
+
+    /**
+     * Creates a new exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The exception message.
+     *
+     * @return RootException The new exception.
+     */
+    public function createException($message = '')
+    {
+        $mock = $this->getMockBuilder('Exception')
+            ->setConstructorArgs([$message])
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
      *
      * @since [*next-version*]
      */
-    public function testGetSetKey()
+    public function testCanBeCreated()
     {
         $subject = $this->createInstance();
-        $reflect = $this->reflect($subject);
 
-        $reflect->_setKey($expected = 'test-key-123');
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
 
-        $this->assertEquals($expected, $reflect->_getKey());
+    /**
+     * Tests whether `_getKey()` works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetKey()
+    {
+        $key = uniqid('key');
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->key = $key;
+        $result = $_subject->_getKey();
+        $this->assertEquals($key, $result, 'Incorrect key retrieved from subject');
+    }
+
+    /**
+     * Tests whether `_setKey()` works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetKey()
+    {
+        $key = uniqid('key');
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->key = null;
+        $_subject->_setKey($key);
+        $result = $_subject->key;
+        $this->assertEquals($key, $result, 'Incorrect key retrieved from subject');
     }
 }
